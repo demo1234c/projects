bff9277dbc35f0867fb9f51ac2518caa5ff725ff
demo1234c <73331625+demo1234c@users.noreply.github.com>
2020-10-23 15:31:29 +0530

    Add files via upload

---
+++ calculator-web-app/calculator-web-app/README.markdown
@@ -0,0 +1,5 @@
+# Calculator Web App
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/CarollynM/pen/YzWGdwK](https://codepen.io/CarollynM/pen/YzWGdwK).
+
+
---
+++ calculator-web-app/calculator-web-app/src/script.js
@@ -0,0 +1,13 @@
+function add() {
+var result =
+Number(document.getElementById("firstno").value) +
+Number(document.getElementById("secondno").value);
+window.alert(result);
+}
+
+function subtract(){
+  var result2 =
+      Number(document.getElementById("subFirstNo").value) -
+ Number(document.getElementById("subSecondNo").value);
+  window.alert(result2);
+}
